e1d3c5efc28f054251d9642b7eed620920bd26a9
Shalya <shalya.mishra20@gmail.com>
2025-12-17 20:49:08 -0600

    Updated main to use gemma3 model under ollama. Also updated pyproject accordingly

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+
 load_dotenv()
 
 
@@ -25,8 +27,8 @@ def main():
         input_variables=["information"], template=summary_template
     )
 
-    # llm = ChatOllama(temperature=0, model="gemma3:270m")
-    llm = ChatOpenAI(temperature=0, model="gpt-5")
+    llm = ChatOllama(temperature=0, model="gemma3:4b")
+    # llm = ChatOpenAI(temperature=0, model="gpt-5")
     chain = summary_prompt_template | llm
 
     response = chain.invoke(input={"information": information})
